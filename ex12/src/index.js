e6d9330bf2b915d0732b6b14e70d4bbd9e127d85
Nihadk12 <kadic_niho@hotmail.com>
2021-09-20 13:44:38 +0200

    comm

---
+++ ex12/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+
+function randomInteger() {
+   total = Math.floor(Math.random() * 30);
+    return total;
+}
+randomInteger();
+console.log(total);
+
+module.exports = randomInteger;
