4967e0d184cfb1cf449306f8b334dbf6c4a96610
Nihadk12 <kadic_niho@hotmail.com>
2021-09-20 11:27:04 +0200

    commited

---
+++ ex03/src/index.js
@@ -0,0 +1,13 @@
+//Write code below this line
+
+//Write code above this line
+
+var notGlobalVariable = 5;
+
+function function1() {
+    var myGlobalVariable = 10;
+
+}
+
+
+module.exports = function1;
