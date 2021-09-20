1dedcb4d9fe4a4ad00d0bd6565c285cd3b8b45ce
Nihadk12 <kadic_niho@hotmail.com>
2021-09-20 16:52:28 +0200

    comm

---
+++ ex14/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line 
+function convertStrToInt(str) {
+
+     a = parseInt(str);
+    return a;
+
+}
+
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("JeffBezos"));
+
+//Only change code above this line 
+
+module.exports = convertStrToInt;
