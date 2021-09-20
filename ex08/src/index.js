808407aa449233e99372076f90f306d4b069ff79
Nihadk12 <kadic_niho@hotmail.com>
2021-09-20 12:34:36 +0200

    comm

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+//Only chage code below this line
+
+var total = 0;
+
+function funcWithArg(digit) {
+    total =  (digit + 9) / 5;
+    return total;
+}
+
+funcWithArg(6);
+
+//Only change code above this line
+
+console.log(total);
+module.exports = funcWithArg;
